9a7d085d97eda8b5ef625d7123fd6c0e2a1e1ca2
Aditya Raj <aditya.x510@gmail.com>
2019-05-19 11:34:09 +0530

    init

--- src/index.php
+++ src/index.php
@@ -15,33 +15,9 @@
 </head>
 <body>
 
-<header class="site-navbar py-4 bg-white" role="banner">
-
-    <div class="container">
-        <div class="row align-items-center">
-
-            <div class="col-11 col-xl-2">
-                <h1 class="mb-0 site-logo"><a href="#" class="text-black h2 mb-0">Notes ki Duniya</a></h1>
-            </div>
-            <div class="col-12 col-md-10 d-none d-xl-block">
-                <nav class="site-navigation position-relative text-right" role="navigation">
-
-                    <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="../Login_v15/index.html">Login</a></li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-        </div>
-
-    </div>
-
-</header>
+<?php
+    include("include/header.php");
+?>
 
 <!--url(images/design2.jpeg);" data-stellar-background-ratio="0.5"-->
 
@@ -68,5 +44,9 @@
 
 </div>
 
+
+<?php
+    include("include/footer.php");
+?>
 </body>
 </html>
