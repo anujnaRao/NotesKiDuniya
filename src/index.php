832990ca0d33aae4715f98648c2acc5a8d03ca1e
Anujna P Rao <46913368+anujnaRao@users.noreply.github.com>
2019-05-19 11:16:00 +0530

    init

---
+++ src/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Notes ki Duniya</title>
+    <meta charset="utf-8">
+
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrapmin.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
+    <link rel="stylesheet" href="../css/jqueryui.css">
+    <link rel="stylesheet" href="../css/carousalmin.css">
+    <link rel="stylesheet" href="../css/defaultmin.css">
+    <link rel="stylesheet" href="../css/style.css"/>
+
+</head>
+<body>
+
+<header class="site-navbar py-4 bg-white" role="banner">
+
+    <div class="container">
+        <div class="row align-items-center">
+
+            <div class="col-11 col-xl-2">
+                <h1 class="mb-0 site-logo"><a href="#" class="text-black h2 mb-0">Notes ki Duniya</a></h1>
+            </div>
+            <div class="col-12 col-md-10 d-none d-xl-block">
+                <nav class="site-navigation position-relative text-right" role="navigation">
+
+                    <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="../Login_v15/index.html">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+
+    </div>
+
+</header>
+
+<!--url(images/design2.jpeg);" data-stellar-background-ratio="0.5"-->
+
+
+<div class="site-blocks-cover overlay">
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-12">
+
+                <div class="row justify-content-center mb-4">
+                    <div class="col-md-8 text-center">
+                        <h1>We Make Notes Useful!<span class="typed-words"></span></h1>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <div><a href="../index.html" class="btn btn-primary btn-md">SignUp!</a></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
