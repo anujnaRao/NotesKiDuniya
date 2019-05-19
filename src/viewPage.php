5ceaf44f3cabc46202c5bcf3c54feeb051920058
anujnaRao <anujna.p.rao@gmail.com>
2019-05-19 12:35:00 +0530

    i

---
+++ src/viewPage.php
@@ -0,0 +1,28 @@
+<?php
+ include("include/header.php");
+?>
+
+<html>
+ <head>
+     <title>View Page</title>
+     <link rel="stylesheet" href="../css/bootstrapmin.css">
+     <link rel="stylesheet" href="../css/magnific-popup.css">
+     <link rel="stylesheet" href="../css/jqueryui.css">
+     <link rel="stylesheet" href="../css/carousalmin.css">
+     <link rel="stylesheet" href="../css/defaultmin.css">
+     <link rel="stylesheet" href="../css/style.css"/>
+ </head>
+ <body>
+   <div class="container">
+      <div class="col-md-6">
+
+      </div>
+       <div class="col-md-6">
+           
+       </div>
+   </div>
+ </body>
+</html>
+<?php
+include ("include/footer.php");
+?>
