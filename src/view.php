9a32d8bd37b83782b794262e86a0669bbc2366e1
Aditya Raj <aditya.x510@gmail.com>
2019-05-19 20:18:34 +0530

    init

--- src/view.php
+++ src/view.php
@@ -1,54 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Notes ki Duniya</title>
-    <meta charset="utf-8">
-
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="../css/bootstrapmin.css">
-    <link rel="stylesheet" href="../css/magnific-popup.css">
-    <link rel="stylesheet" href="../design/bootstrapmin.css">
-    <script src="../design/jquerymin.js"></script>
-    <script src="../design/bootstrapmin.js"></script>
-    <script src="../design/poppermin.js"></script>
-    <link rel="stylesheet" href="../css/jqueryui.css">
-    <link rel="stylesheet" href="../css/carousalmin.css">
-    <link rel="stylesheet" href="../css/defaultmin.css">
-    <link rel="stylesheet" href="../css/style.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="./jquery-3.4.0.slim.min.js"></script>
+    <script src="./bootstrap.js"></script>
+    <script src="./popper.min.js"></script>
+    <script src="./tooltip.min.js"></script>
+    <title>Login</title>
 
 </head>
 <body>
-
-<section class="card-header">
-    <header class="site-navbar py-4 bg-white" role="banner">
-
-        <div class="container">
-            <div class="row align-items-center">
-
-                <div class="col-11 col-xl-2">
-                    <h1 class="mb-0 site-logo"><a href="#" class="text-black h2 mb-0 ">Notes ki Duniya</a></h1>
-                </div>
-                <div class="col-12 col-md-10 d-none d-xl-block">
-                    <nav class="site-navigation position-relative text-right" role="navigation">
-
-                        <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-                            <li class="nav-item ">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
-
-
-                        </ul>
-                    </nav>
-                </div>
-
-                <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-            </div>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light static-top container-fluid">
+    <div class="container">
+        <a class="navbar-brand logo" href="index.html">
+            <img src="ll.png" class="img-fluid " alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto text-center text-justify">
+                <li class="nav-item ">
+                    <a class="nav-link" href="index.php">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="view.php">View</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../home/regform/Login_v15/vendor/login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+            </ul>
         </div>
+    </div>
+</nav>
 
-    </header>
-</section>
+<!--<section class="card-header">-->
+<!--    <header class="site-navbar py-4 bg-white" role="banner">-->
+<!---->
+<!--        <div class="container">-->
+<!--            <div class="row align-items-center">-->
+<!---->
+<!--                <div class="col-11 col-xl-2">-->
+<!--                    <h1 class="mb-0 site-logo"><a href="#" class="text-black h2 mb-0 ">Notes ki Duniya</a></h1>-->
+<!--                </div>-->
+<!--                <div class="col-12 col-md-10 d-none d-xl-block">-->
+<!--                    <nav class="site-navigation position-relative text-right" role="navigation">-->
+<!---->
+<!--                        <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">-->
+<!--                            <li class="nav-item ">-->
+<!--                                <a class="nav-link" href="index.php">Home</a>-->
+<!--                            </li>-->
+<!--                            <li class="nav-item active">-->
+<!--                                <a class="nav-link" href="view.php">View</a>-->
+<!--                            </li>-->
+<!--                            <li class="nav-item ">-->
+<!--                                <a class="nav-link" href="../home/regform/Login_v15/vendor/login.php">Login</a>-->
+<!--                            </li>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--                        </ul>-->
+<!--                    </nav>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>-->
+<!---->
+<!--            </div>-->
+<!---->
+<!--        </div>-->
+<!---->
+<!--    </header>-->
+<!--</section>-->
 
 <section>
     <div class="container-fluid">
